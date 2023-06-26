83468a9b09d52aa7e4205e9a757d7a3acf9db7fd
gustavocodigo <gilapesgus@gmail.com>
2023-06-25 21:51:47 -0300

    Added products api

--- js/produtos.js
+++ js/produtos.js
@@ -58,36 +58,47 @@ function update_filter(name) {
         element.element.style.display = "none"
     }
 
-    setTimeout(function () {
+    setTimeout(async function () {
         loading_element.style.display = "none"
-        let len = 0;
-        for (let index = 0; index < products_list.length; index++) {
-            const element = products_list[index];
-            if (element.name.toLowerCase().includes(name.toLowerCase())){
-                len++;
-                
-                element.element.style.display = "block"
-            }else
-                element.element.style.display = "none"
-            search_locked = false
-            if ( name != "")
-            document.getElementById("results-for-text").style.display = "block"
-            let texto = len+" Resultados para: "+name
-            if (len == 0) {
-                texto = "Sem resultados para: "+name
-            }else if ( len == 1) {
-                texto = "Foi encontrado 1 resultado para: "+name
-            }
-            document.getElementById("results-for-text").innerText = texto
+       
+        const products = await get_products(name)
+        const len = products.length
 
+        products_list.map( (product) => {
+            product.element.remove()
+        })
+        products_list = []
+
+        for (let index = 0; index < products.length; index++) {
+            const element = products[index];
+            add_to_cart(element)
         }
+
+        let texto = len + " Resultados para: " + name
+        if (len == 0) {
+            texto = "Sem resultados para: " + name
+        } else if (len == 1) {
+            texto = "Foi encontrado 1 resultado para: " + name
+        }
+
+        document.getElementById("results-for-text").innerText = texto
+        if ( name != "")
+        document.getElementById("results-for-text").style.display = "block"
+        search_locked = false
     }, 2000)
 
 }
 
 
+const url = "https://academia-x-api.glitch.me"
 
 
+async function get_products(name) {
+    name = name || ""
+    const response = await fetch(`${url}/products?search=${encodeURIComponent(name)}`);
+    const products = await response.json();
+    return products
+}
 
 
 
@@ -96,54 +107,16 @@ function domLoaded() {
     produto_template.style.display = "none"
     list = document.getElementById("product-list")
     document.getElementById("results-for-text").style.display = "none"
-    const products = [
-        {
-            "price": "312,00 R$",
-            "name": "INTEGRITY+ TREADMILL",
-            "image": "https://www.lifefitness.com/resource/image/2323806/portrait_ratio1x1/400/400/7dd2288b7752d5c03eb03fe195226832/ER/life-fitness-integrity-series-treadmill-with-24-inch-se4-in-matte-black.png"
-        },
-        {
-            "price": "300,00 R$",
-            "name": "TOTAL BODY ARC TRAINER",
-            "image": "https://www.lifefitness.com/resource/image/2323834/portrait_ratio1x1/400/400/d4f7d5e88c3da28086f3f554c014be7e/MD/life-fitness-integrity-series-total-body-arc-with-24-inch-se4-in-black-onyx.png"
-        },
-        {
-            "price": "3000,00 R$",
-            "name": "ASPIRE TREADMILL",
-            "image": "https://www.lifefitness.com/resource/image/2272264/portrait_ratio1x1/400/400/a0fa251b9651a66222cef00343427707/DZ/aspire-treadmill-sl-console-smooth-charcoal.png"
-        },
-        {
-            "price": "1234,00 R$",
-            "name": "INTEGRITY+ ELLIPTICAL CROSS-TRAINER",
-            "image": "https://www.lifefitness.com/resource/image/2323812/portrait_ratio1x1/400/400/bda61656e4cafb677c3155fb0ada278f/fN/life-fitness-integrity-series-elliptical-cross-trainer-with-24-inch-se4-in-black-onyx.png"
-        },
-        {
-            "price": "1120,00 R$",
-            "name": "IC7 INDOOR CYCLEL",
-            "image": "https://www.lifefitness.com/resource/image/2304864/portrait_ratio1x1/400/400/f4ec30b7055c253347c16892323ed587/ST/icg-ic7-bg-free.png"
-        },
-        {
-            "price": "1310,00 R$",
-            "name": "INTEGRITY+ LIFECYCLE UPRIGHT BIKE",
-            "image": "https://www.lifefitness.com/resource/image/2323830/portrait_ratio1x1/400/400/2f9c8d4451fe2ddd27e741575c725fb4/CB/life-fitness-integrity-series-upright-bike-with-24-inch-se4-in-black-onyx.png"
-        },
-        {
-            "price": "131012,00 R$",
-            "name": "INTEG2RITY+ LIFECYCLE UPRIGHT BIKE",
-            "image": "https://www.lifefitness.com/resource/image/2323830/portrait_ratio1x1/400/400/2f9c8d4451fe2ddd27e741575c725fb4/CB/life-fitness-integrity-series-upright-bike-with-24-inch-se4-in-black-onyx.png"
+
+    get_products().then(products => {
+        for (let index = 0; index < products.length; index++) {
+            const element = products[index];
+            add_to_cart(element)
         }
-    ]
+        console.table(products)
+        document.getElementById("laoding-icon").style.display = "none"
 
-
-    for (let index = 0; index < products.length; index++) {
-        const element = products[index];
-        add_to_cart(element)
-    }
-    
-
-    console.table(products)
-
-
+    }).catch(e => console.log(e))
 }
 
 
