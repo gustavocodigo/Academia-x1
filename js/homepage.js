d62553dcd9b423b96d7adef940202fe70f622f2e
gustavocodigo <gilapesgus@gmail.com>
2023-06-26 07:21:47 -0300

    Added nav bar mobile

---
+++ js/homepage.js
@@ -0,0 +1,9 @@
+
+
+function toggle_nav() {
+    if ( document.getElementById("nav").style.display == "none") {
+        document.getElementById("nav").style.display = "flex"
+    }else {
+        document.getElementById("nav").style.display = "none"
+    }
+}
--- js/produtos.js
+++ js/produtos.js
@@ -120,4 +120,10 @@ function domLoaded() {
 }
 
 
-
+function toggle_nav() {
+    if ( document.getElementById("nav").style.display == "none") {
+        document.getElementById("nav").style.display = "flex"
+    }else {
+        document.getElementById("nav").style.display = "none"
+    }
+}
